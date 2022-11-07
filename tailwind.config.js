ff20be862d4a134ddd52b9f41fb0fa0532e8b6de
Bala Gopinaaath <balagopinaathm@venzotechnologies.com>
2022-11-07 20:05:12 +0530

    staticPage

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'titles': ['Bebas Neue'],
+        'small': ['Barlow', 'sans-serif'],
+      },
+      colors: {
+        'tred': '#ff0019'
+      }
+    },
+  },
+  plugins: [],
+}
